e3326f319a6248f8c6da11da4950a563fd1b5a25
Juliette Chevalier <juliettech.1393@gmail.com>
2023-01-30 15:28:31 -0500

    adding sdk calls to deposit funds as well as to mint tokens

--- src/components/FundraisingModal/index.tsx
+++ src/components/FundraisingModal/index.tsx
@@ -1,25 +1,60 @@
-import React, { MouseEvent } from 'react';
+import React, { Dispatch, MouseEvent, SetStateAction, useState } from 'react';
+import { ethers } from 'ethers';
+import { useSigner } from 'wagmi';
 
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
 
+import { DaoDepositSteps } from "@aragon/sdk-client";
+import { depositETH, mintToken, useAragonSDKContext } from '../../context/aragon-sdk';
+
 import { STRINGS } from '../../strings/english';
 
 interface Params {
-  show: boolean;
-  handleClose: () => void;
+  setAlertText: Dispatch<SetStateAction<string>>;
+  setAlertVariant: Dispatch<SetStateAction<'success' | 'danger'>>;
+  showModal: boolean;
+  setShowAlert: Dispatch<SetStateAction<boolean>>;
+  setShowModal: Dispatch<SetStateAction<boolean>>;
 }
 
 export default function FundraisingModal(params: Params): JSX.Element {
-  const { show, handleClose } = params;
+  const { setAlertText, setAlertVariant, showModal, setShowAlert, setShowModal } = params;
+
+  const [amountToDeposit, setAmountToDeposit] = useState<number>(0);
+
+  const { client } = useAragonSDKContext();
+  const signer = useSigner().data || undefined;
 
   async function handleSubmit(event: MouseEvent) {
     event?.preventDefault();
+
+    const DAODepositSteps = await depositETH(client, amountToDeposit);
+    for await (const step of DAODepositSteps) {
+      try {
+        switch (step.key) {
+          case DaoDepositSteps.DEPOSITING:
+            setShowModal(false);
+            setAlertText(`Depositing ${amountToDeposit} ETH into ParksDAO. Here is your transaction hash: ${step.txHash}`);
+            setShowAlert(true);
+            break;
+          case DaoDepositSteps.DONE:
+            setAlertText(`Successfully deposited ${ethers.utils.formatEther(step.amount)} ETH into ParksDAO!`);
+            setShowAlert(true);
+            mintToken(signer);
+            break;
+        }
+      } catch (error) {
+        setAlertText(`${error}`);
+        setAlertVariant('danger');
+        setShowAlert(true);
+      }
+    }
   }
 
   return (
-    <Modal show={show} onHide={handleClose}>
+    <Modal show={showModal} onHide={() => setShowModal(false)} backdrop>
       <Modal.Header closeButton>
         <Modal.Title>
           <h4>{STRINGS.contribute}</h4>
@@ -30,7 +65,15 @@ export default function FundraisingModal(params: Params): JSX.Element {
 
         <Form>
           <Form.Group className="mb-3" controlId="amountOfETH">
-            <Form.Control type="number" placeholder={STRINGS.enterETHAmount} />
+            <Form.Control
+              type="number"
+              min="0"
+              max="1000"
+              step="any"
+              placeholder={STRINGS.enterETHAmount}
+              value={amountToDeposit}
+              onChange={(event) => setAmountToDeposit(parseFloat(event.target.value))}
+            />
             <Form.Text className="text-muted">
               {STRINGS.onlyETH}
             </Form.Text>
--- src/context/aragon-sdk.tsx
+++ src/context/aragon-sdk.tsx
@@ -1,13 +1,15 @@
-import { Context as ReactContext, createContext, useEffect, useContext, useState } from 'react';
+import { createContext, useEffect, useContext, useState } from 'react';
 
 import { useSigner } from 'wagmi';
-import { Client, Context, ContextParams } from '@aragon/sdk-client';
+import { Client, Context, ContextParams, ContextPlugin, IDepositParams, IMintTokenParams, TokenVotingClient } from '@aragon/sdk-client';
+
+import { ETHToWei } from '../helpers/crypto';
 
 interface AragonSDKWrapperContext {
   children: JSX.Element;
 }
 
-const AragonSDKContext: ReactContext<{}> = createContext({});
+const AragonSDKContext = createContext({});
 
 export function AragonSDKWrapper({ children }: AragonSDKWrapperContext): JSX.Element {
   const [client, setClient] = useState<Client | undefined>(undefined);
@@ -39,6 +41,45 @@ export function AragonSDKWrapper({ children }: AragonSDKWrapperContext): JSX.Ele
   )
 }
 
-export function useAragonSDKContext() {
+export function useAragonSDKContext(): any {
   return useContext(AragonSDKContext);
+}
+
+export function depositETH(client: Client, amountToDeposit: number) {
+  const depositParams: IDepositParams = {
+    daoAddressOrEns: "0xae8586ee1ef50544683b6d9d608ff920ab081357",
+    amount: ETHToWei(amountToDeposit),
+    reference: "ETH deposit"
+  };
+
+  return client.methods.deposit(depositParams);
+}
+
+export async function mintToken(signer: any) {
+  const aragonSDKContextParams: ContextParams = {
+    network: 'goerli',
+    signer,
+    daoFactoryAddress: '0x66DBb74f6cADD2486CBFf0b9DF211e3D7961eBf9',
+    web3Providers: ['https://rpc.ankr.com/eth_goerli'],
+    ipfsNodes: [
+      {
+        url: 'https://testing-ipfs-0.aragon.network/api/v0',
+        headers: { 'X-API-KEY': process.env.REACT_APP_IPFS_KEY || '' }
+      },
+    ],
+    graphqlNodes: [{ url: 'https://subgraph.satsuma-prod.com/aragon/core-goerli/api' }]
+  };
+
+  const context: Context = new Context(aragonSDKContextParams);
+  const contextPlugin: ContextPlugin = ContextPlugin.fromContext(context);
+  const client = new TokenVotingClient(contextPlugin);
+
+  const params: IMintTokenParams = {
+    address: signer.address,
+    amount: BigInt(10)
+  };
+
+  const minterAddress = "0x0987654321098765432109876543210987654321";
+  const action = await client.encoding.mintTokenAction(minterAddress, params);
+  console.log(action);
 }
